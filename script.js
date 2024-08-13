96cc27075c3c86e2a8c9947e57d8069024586b48
Laxman kumar <laxmankr042@gmail.com>
2024-08-13 20:35:46 +0530

    Changes Done

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const barIcon = document.querySelector(".bar-icon");
-const linksDiv = document.querySelector(".links-div");
-const links = document.querySelectorAll(".nav-links");
+const menuIcon = document.querySelector(".menu-icon");
+const rightDiv = document.querySelector(".nav-right-div");
+const navLinks = document.querySelectorAll(".nav-links");
 let body = document.querySelector("body");
 let isBarIconChecked = true;
 
-barIcon.addEventListener("click",() => {
-    if(isBarIconChecked === true){
-        linksDiv.style.display = "flex";
-        barIcon.innerHTML = `<i class="fa-solid fa-xmark" style="color: #3030ff;"></i>`;
-        isBarIconChecked = false;
-    }
-    else {
-        linksDiv.style.display = "none";
-        barIcon.innerHTML = `<i class="fa-solid fa-bars"></i>`;
-        isBarIconChecked = true;
-    }
-})
-links.forEach((link)=>{
-    link.addEventListener("click",()=>{
-        linksDiv.style.display = "none";
-        barIcon.innerHTML = `<i class="fa-solid fa-bars"></i>`;
-        isBarIconChecked = true;
-    })
-})
+menuIcon.addEventListener("click", () => {
+  if (isBarIconChecked === true) {
+    rightDiv.style.translate = "0";
+    menuIcon.innerHTML = `<i class="fa-solid fa-xmark" style = 'color : #fbfcf8'></i>`;
+    body.style.overflow = "hidden";
+    isBarIconChecked = false;
+  } else {
+    rightDiv.style.translate = "100%";
+    menuIcon.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+    body.style.overflow = "auto";
+    isBarIconChecked = true;
+  }
+});
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    body.style.overflow = "auto";
+    rightDiv.style.translate = "100%";
+    menuIcon.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+    isBarIconChecked = true;
+  });
+});
